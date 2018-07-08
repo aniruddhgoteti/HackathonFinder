85892210b4718b2f42eea34f5e9f5d764105fe14
Aniruddh Goteti <aniruddhgoteti@gmail.com>
2018-07-08 23:27:51 +0200

    Added Maps

--- Scraping/hackalist.org/hackalistorg.py
+++ Scraping/hackalist.org/hackalistorg.py
@@ -11,7 +11,6 @@ from itertools import repeat
 from geopy.geocoders import Nominatim
 
 geolocator = Nominatim()
-
 Client= MongoClient('localhost', 27017)
 db= Client["hackathons"]
 
@@ -19,7 +18,7 @@ hack= db.hack_finder
 
 
 driver = webdriver.Chrome()
-driver.get("http://hackathonsnear.me/")
+driver.get("http://www.hackalist.org")
 sleep(4)
 
 title_ = []
@@ -27,58 +26,60 @@ dates_= []
 loc_= []
 gps_ = []
 url_=[]
+sponsor_= []
+
 null = None
 
-title= driver.find_elements_by_xpath('//a[@class="hackathon-name ng-binding"]')
-
-
-sel= Selector(text= driver.page_source)	
-dates= sel.xpath('//div[@class="columns small-3 medium-2 large-3 ng-binding"]//text()').extract()
-
-extract_url=sel.xpath('//a/@href').extract()
-
-url= list(OrderedDict.fromkeys(extract_url))
-#url_=url[4:-4]
-url_= url[4:-4]
-
-location= sel.xpath('//div[@class="columns small-4 medium-3 large-2 ng-binding"]//text()').extract()
+title= driver.find_elements_by_xpath('//h2[@class="ng-binding"]')
 
 for x in range(len(title)):
 	title_.append(title[x].text)
-	dates_.append(dates[x].strip())
+
+
+sel= Selector(text= driver.page_source)	
+
+location= sel.xpath('//p[@class="info-line ng-binding"]//text()').extract()
+
+sponsor= sel.xpath('//p[@class="info-line-sm ng-binding"]//text()').extract()
+
 
 
 for x in range(len(location)):
-	if len(location[x].strip())>6:
-		loc_.append(location[x].strip())
-	
+	loc_.append(location[x].strip()[21:-1])
+
+#for x in range(len(des)):
+#	des_.append(des[x].strip())
+
+
+for x in range(len(sponsor)):
+	sponsor_.append(sponsor[x].strip())
+
 for x in range(len(loc_)):
 	try:
 		gps_.append(str(geolocator.geocode(loc_[x]).latitude)+ "," + str(geolocator.geocode(loc_[x]).longitude))
 	except AttributeError:
 		gps_.append(null)
-	
 
-title_ = list(OrderedDict.fromkeys(title_))
-#list(set(title_))	
 
-dates_ = list(OrderedDict.fromkeys(dates_))
-#list(set(dates_))
-		
 
-#print(len(title_), len(dates_), len(loc_), len(gps_), len(url_))
-#print(url_)
-#print(title_)
+url= sel.xpath('//a/@href').extract()
 
+url_.append(url[5])
+url_.append(url[6])
+url_.append(url[9])
+url_.append(url[12])
+url_.append(url[-5])
+url_.append(url[-1])
 
 db_data= pd.DataFrame(
     {'Image': null,
      'Title of event': title_,
-     'Date': dates_,
+     'Date': null,
      'Time': null,
      'Location': loc_,
      'GPS': gps_,
      'Event Description': null,
+     'sponsor': sponsor_,
      'Event URL': url_
     })
 
@@ -86,3 +87,10 @@ records = json.loads(db_data.T.to_json()).values()
 hack.insert(records)
 
 driver.quit()
+
+
+		
+
+
+
+
--- Scraping/list_view.py
+++ Scraping/list_view.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask_pymongo import PyMongo
 from flask import jsonify
+import pandas as pd
+import os
+import pandas as pd
+import numpy as np
+from IPython.core.display import display, HTML
+import pymongo
+from pymongo import MongoClient
+from pymongo import DeleteMany
+import folium
+from urlparse import urljoin
+from werkzeug.contrib.atom import AtomFeed
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+
+api = Api(app)
+
+Client= MongoClient('localhost', 27017)
+db= Client["hackathons"]
+
+hack= db.hack_finder
 
 app= Flask(__name__)
 
@@ -9,12 +30,10 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/hackathons'
 
 mongo=PyMongo(app)
 
-
 @app.route('/list')
-
-def printList():
+def getList():
+	
 	list=mongo.db.hack_finder
-
 	list_find= list.find()
 	result= []
 
@@ -30,5 +49,87 @@ def printList():
 	return jsonify(result)	
 
 
+@app.route('/map')
+def getmap():
+	with app.app_context():
+		global hackmap
+		list=mongo.db.hack_finder
+		list_find= list.find()
+		result= []
+		gps_= []
+		null= None
+
+		for j in list_find:
+			result.append({'Title of event': j['Title of event'],
+    		# 'Date': j['Date'],
+    		# 'Time': j['Time'],
+     		'Location': j['Location'],
+     		'GPS': j['GPS'],
+    		# 'Event Description': j['Event Description'],
+     		'Event URL': j['Event URL']})
+	
+	
+		for x in range(len(result)):
+			try:
+		
+				if type(result[x]["GPS"][0])== float:
+					gps_.append(str(result[x]["GPS"][0])+","+str(result[x]["GPS"][1]))
+				else:
+					gps_.append(result[x]["GPS"])
+				
+			except TypeError:
+				gps_.append(None)
+				
+
+
+		
+		gps_ = pd.Series(gps_)		
+
+
+		df1 = pd.DataFrame(result)
+		df1.drop(['GPS'], axis=1)
+		df1['GPS'] = gps_.values
+
+		Lat= []
+		Long=[]
+		Labels= []
+
+		for x in range(len(df1)):
+			try:
+				Lat.append(float(df1.GPS[x].split(',')[0][0:5]))
+				Long.append(float(df1.GPS[x].split(',')[1][0:5]))
+				Labels.append(df1["Title of event"][x])
+			except AttributeError:
+				pass
+
+		Lat = pd.Series(Lat)
+		Long= pd.Series(Long)
+
+		df = pd.DataFrame()
+		df["Lat"]= Lat.values
+		df["Long"]= Long.values
+
+		locationlist = df[["Lat","Long"]].values.tolist()
+
+		hackmap = folium.Map(location=[35, 100],tiles='Stamen Toner', zoom_start=14)
+		for point in range(len(locationlist)):
+			popup = folium.Popup(Labels[point], parse_html=True)
+			folium.Marker(locationlist[point], popup=popup).add_to(hackmap)
+		
+
+		return hackmap.get_root().render()
+
+		#hackmap.save('newmap.html')
+
+		#return app.send_static_file('newmap.html')
+  
+
+
+#api.add_resource(HackathonList, '/list') # Route_1
+#api.add_resource(Map, '/map') # Route_2
+#api.add_resource(Calemdar, '/cal') # Route_3
+#api.add_resource(Feed, '/feed') # Route_3
+
 if __name__=='__main__':
+	
 	app.run(debug=True)
